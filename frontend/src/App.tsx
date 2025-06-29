58a476ab9e7187fe4ea46bf87734dc5bd6b9dc29
Gabriel Schimidt <gabrielbernardinii@gmail.com>
2025-06-29 20:38:20 -0300

    feat: modificações - frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Students from './pages/Students'
 import Navbar from './components/Navbar'
+import CheckinPage from './pages/checkin.page'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/students" element={<Students />} />
+          <Route path="/checkin" element={<CheckinPage />} />
         </Routes>
       </div>
     </Router>
